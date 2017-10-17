5c834e29d83f619a6c2bf1d70bbff31e3d60cd8e
Dion Arya Pamungkas <dionarya.p@gmail.com>
2017-10-17 18:54:26 +0700

    Update Tugas_rental_buku.c

--- Tugas_rental_buku.c
+++ Tugas_rental_buku.c
@@ -6,13 +6,8 @@ Program Rental Buku
 #include <stdio.h>
 #include <conio.h>
 int lama_menyewa; int denda;
-char nama[20] = "Follow @dion_arya.p :D, Sang pembuat program :D";
-void watermark(){
-	printf("%c"), nama;
-}
 main(){
 clrscr();
-watermark();
 
 printf("Selamat datang di Rental Buku Hallo\n");
 printf("Masukan Lama Anda Menyewa : ");scanf("%d", &lama_menyewa);
@@ -21,10 +16,10 @@ if(lama_menyewa > 3){
 denda = lama_menyewa*2000;
 printf("Anda Sudah Lebih dari 3 hari tidak membalikan buku\n");
 printf("Anda Dikenakan denda dan harus membayar %d", denda);
-watermark();
+
 }else{
 printf("Anda Menyewa Buku nya selama %d Hari\n terimakasih telah membalikan bukunya", lama_menyewa);
-watermark();
+
 }
 
 getch();
